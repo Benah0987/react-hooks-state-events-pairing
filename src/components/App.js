7bab2d5ebaeb3aea559881e2bb58ba73074f61d7
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-07 13:21:27 +0300

    adding a btn to handle display comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import video from "../data/video.js";
+import React, {useState} from 'react'
 
 function App() {
-  console.log("Here's your data:", video);
+ 
+  //commments
+  const [comments, setComment] = useState(false)
 
+  //function to handle the change in comments
+  function handleClick(){
+    setComment(!comments)
+  }
+ 
   return (
-    <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+    <div className="container-fluid bg-success">
+     
     </div>
   );
 }
