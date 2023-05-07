bf65ab873a5c297ae99a9575b906627dc217774c
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-07 13:34:15 +0300

    adding  a showVid component

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,24 @@ import React, {useState} from 'react'
 function App() {
  
   //commments
-  const [comments, setComment] = useState(false)
+  const [seeComments, setSeeComment] = useState(false)
 
   //function to handle the change in comments
   function handleClick(){
-    setComment(!comments)
+    setComment(!seeComments)
   }
  
   return (
     <div className="container-fluid bg-success">
-     
+      {/*where i will display the video*/}
+      <ShowVid video = {video} 
+              toggle = {handleClick} 
+              seeComments = {seeComments} 
+       />
+
+       <Comment toggleHide = {seeComments}
+               comments= {video.comments}
+        />       
     </div>
   );
 }
